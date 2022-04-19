e374f08cea85d5f11698bee39f5e7a723ea7c9fe
soryeongk <soryeongk.kr@gmail.com>
2022-04-19 23:42:57 +0900

    :hammer: refactor: 함수 분리 후 exports

--- problems/가장큰수/소령/solution.js
+++ problems/가장큰수/소령/solution.js
@@ -1,12 +1,9 @@
-function compareStringNumbers(a, b) {
-  const num1 = a.toString();
-  const num2 = b.toString();
-  
-  return (num2 + num1) - (num1 + num2);
-}
+const compareStringNumbers = require("./compareStringNumbers");
 
 function solution(numbers) {
   numbers.sort((a, b) => compareStringNumbers(a, b));
 
   return numbers[0] === 0 ? '0' : numbers.join('');
 }
+
+module.exports = solution;
