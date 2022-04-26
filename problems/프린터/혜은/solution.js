5999173d9f90ebdade9d4b92068e8f740fc0a075
Hennization <shy6295@ewhain.net>
2022-04-27 03:20:33 +0900

    리드미 추가

--- problems/프린터/혜은/solution.js
+++ problems/프린터/혜은/solution.js
@@ -1,5 +1,18 @@
 function solution(priorities, location) {
   let answer = 0;
+
+  // let printList = [];
+  // for (let i = 1; i < priorities.length; i++) {
+  //   for (let j = 1; j < priorities.length; j++) {
+  //     if (priorities[0] >= priorities[j]) {
+  //       printList.push(priorities[0]);
+  //     } else {
+  //       priorities.push(priorities[0]);
+  //     }
+  //     priorities.shift();
+  //   }
+  // }
+
   let findNum = priorities[location]; // 구조분해 할당으로 찾고싶은 수 변수에 담기
 
   const sortedPriorities = priorities.sort((a, b) => b - a); // 정렬
