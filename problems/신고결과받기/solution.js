0385af8972a88be5c69a277e1ce880d4272e7a0f
Hennization <shy6295@ewhain.net>
2022-06-08 20:18:23 +0900

    feat: 기능구현 / 시간 초과

--- problems/신고결과받기/solution.js
+++ problems/신고결과받기/solution.js
@@ -6,7 +6,7 @@ function solution(id_list, report, k) {
     //report에서 중복 제거하기
     const removeRepeat = [...new Set(report)];
     // 신고당한 횟수 배열 만들기
-    id_list.map((id) => {
+    id_list.forEach((id) => {
         // id와 reportedId가 같을 때 해당 아이디 신고당한 횟수에 +1
         let reportedNum = 0;
         removeRepeat.forEach((reportElement) => {
@@ -14,15 +14,21 @@ function solution(id_list, report, k) {
             if (reportedId !== id) return;
             reportedNum++;
         });
+        // 단일 반복문으로 만들어야 함 -> 단일 반복문 뭐로?
+        // id를 돌리는 게 문제다 => report를 돌려서 저장해야한다.
+        // 걍 배열을 저장하는 방식이 틀림.
+        // 무작정 push가 아니라 빈배열이 아니라, index나 키로 접근해야함
+        // id 업이도 돌아가는 방법을 생각해야함.
+        // 단지 시간초과 때문에 이생각
+        // 여기서 틀린 것을 어떻게 고치는지 메모하기
         reportNumArr.push(reportedNum);
     });
     // k번 이상일 때 정지된 ID 배열 만들기
     reportNumArr.forEach((reportNum, idx) => {
         if (reportNum >= k) suspensionIdArr.push(id_list[idx]);
     });
-
     // id_list를 돌면서 결과메일 받을 id 확인하기
-    id_list.map((id) => {
+    id_list.forEach((id) => {
         let resultMailCount = 0;
         removeRepeat.forEach((reportElement) => {
             const [reportId, reportedId] = reportElement.split(" ");
